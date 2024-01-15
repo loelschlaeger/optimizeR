8ab28702388569e47b3053e0ab463c35f37595a3
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2024-01-15 10:08:58 +0100

    skip test for time limit if not on Windows OS

--- tests/testthat/test-optimizer.R
+++ tests/testthat/test-optimizer.R
@@ -56,6 +56,7 @@ test_that("error can be detected", {
 })
 
 test_that("exceed of time limit can be detected", {
+  skip_if_not(.Platform$OS.type == "windows")
   slow_optimizer <- define_optimizer(
     .optimizer = function(f, p, ...) {
       Sys.sleep(2)
