2e7c0c3e0fa0b8e8e5c0e9ec860b43323b23c3e3
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2025-04-20 12:06:26 +0200

    update

--- R/optimizeR-package.R
+++ R/optimizeR-package.R
@@ -2,9 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom checkmate check_function
 #' @importFrom cli cli_abort
 #' @importFrom cli cli_warn
 #' @importFrom lbfgsb3c lbfgsb3c
+#' @importFrom oeli check_missing
 #' @importFrom R6 R6Class
 #' @importFrom stats nlm
 #' @importFrom stats nlminb
